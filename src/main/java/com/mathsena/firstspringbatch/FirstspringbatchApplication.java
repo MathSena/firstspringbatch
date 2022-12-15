701eb9d95e20e2040743ef4f213cb471bd57a4d3
Matheus Sena <MathSena07@hotmail.com>
2022-12-15 13:00:52 -0300

    Initial commit

---
+++ src/main/java/com/mathsena/firstspringbatch/FirstspringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.firstspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathsena/firstspringbatch/FirstspringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.firstspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
